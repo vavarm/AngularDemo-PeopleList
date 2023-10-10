64d952689185a5753fe76b02d15c4a65bc8056eb
vavarm <v.racaud-minuzzi@orange.fr>
2023-10-10 19:20:35 +0200

    ref: rename people counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { PeopleNumberComponent } from './components/people-number/people-number.component'
+import { PeopleNumberComponent } from './components/people-count/people-count.component'
 import { PeopleListComponent } from './components/people-list/people-list.component'
 import { PeopleItemComponent } from './components/people-item/people-item.component'
 import { AddPersonComponent } from './components/add-person/add-person.component'
--- src/app/components/people-count/people-count.component.ts
+++ src/app/components/people-count/people-count.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'people-number',
-  templateUrl: './people-number.component.html',
-  styleUrls: ['./people-number.component.css']
+  templateUrl: './people-count.component.html',
+  styleUrls: ['./people-count.component.css']
 })
 export class PeopleNumberComponent {
   @Input() numberOfPeople!: number
